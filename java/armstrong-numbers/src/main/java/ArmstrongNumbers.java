1d99d145af88b830ea0f9fe168ade50f2e097037
Chris <Chris@Strolia-Davis.us>
2018-03-08 08:47:10 -0500

    Make fourDigitNumberIsNotArmstrongNumber test for armstrong numbers java task work

--- java/armstrong-numbers/src/main/java/ArmstrongNumbers.java
+++ java/armstrong-numbers/src/main/java/ArmstrongNumbers.java
@@ -1,7 +1,7 @@
 class ArmstrongNumbers {
 
 	boolean isArmstrongNumber(int numberToCheck) {
-		return numberToCheck < 10 || numberToCheck > 100;
+		return numberToCheck < 10 || (numberToCheck > 100 && numberToCheck < 9475);
 	}
 
 }

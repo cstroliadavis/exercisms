25906a0fca7264e7d6c15a05f804fc5ac1b23d27
Chris <Chris@Strolia-Davis.us>
2018-03-08 10:15:40 -0500

    Make testRnaTranscriptionOfThymineIsAdenine test for rna transcription java task work

--- java/rna-transcription/src/main/java/RnaTranscription.java
+++ java/rna-transcription/src/main/java/RnaTranscription.java
@@ -1,7 +1,19 @@
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 class RnaTranscription {
+    private static final Map<String, String> CONVERSIONS = buildConversions();
 
     String transcribe(String dnaStrand) {
-        return dnaStrand.equals("C") ? "G" : "C";
+        return CONVERSIONS.get(dnaStrand);
     }
 
+    private static Map<String, String> buildConversions() {
+        Map<String, String> result = new HashMap<>();
+        result.put("C", "G");
+        result.put("G", "C");
+        result.put("T", "A");
+        return Collections.unmodifiableMap(result);
+    }
 }

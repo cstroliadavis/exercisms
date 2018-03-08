18f6f98ccabab6790b6c322f2758e39cd66d9192
Chris <Chris@Strolia-Davis.us>
2018-03-08 10:18:09 -0500

    Make testRnaTranscriptionOfAdenineIsUracil test for rna transcription java task work

--- java/rna-transcription/src/main/java/RnaTranscription.java
+++ java/rna-transcription/src/main/java/RnaTranscription.java
@@ -14,6 +14,7 @@ class RnaTranscription {
         result.put("C", "G");
         result.put("G", "C");
         result.put("T", "A");
+        result.put("A", "U");
         return Collections.unmodifiableMap(result);
     }
 }

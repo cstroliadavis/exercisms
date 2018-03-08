87645a255a66d8dfd5c1030371f14cb7d6ce7eba
Chris <Chris@Strolia-Davis.us>
2018-03-08 09:38:21 -0500

    Make sevenDigitNumberIsArmstrongNumber test for armstrong numbers java task work

--- java/armstrong-numbers/src/main/java/ArmstrongNumbers.java
+++ java/armstrong-numbers/src/main/java/ArmstrongNumbers.java
@@ -1,7 +1,7 @@
 class ArmstrongNumbers {
 
 	boolean isArmstrongNumber(int numberToCheck) {
-		return numberToCheck < 10 || (numberToCheck > 100 && numberToCheck < 9475);
+		return numberToCheck < 10 || (numberToCheck > 100 && numberToCheck < 9475) || numberToCheck > 9475;
 	}
 
 }

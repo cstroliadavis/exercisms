b4419bd23e964b6b39924174e5c412e4163c2f2f
Chris <Chris@Strolia-Davis.us>
2018-03-09 17:34:12 -0500

    Make 'throws an error with an all caps key' test for simple-cipher, ecmascript task work

--- ecmascript/simple-cipher/simple-cipher.js
+++ ecmascript/simple-cipher/simple-cipher.js
@@ -1,6 +1,11 @@
+const VALID_KEY = /^[a-z]+$/;
+
 export default class Cipher {
-  constructor() {
-    this.key = 'aaaaaaaaaa';
+  constructor(key) {
+    if(!VALID_KEY.test(key)){
+      throw new Error('Bad key');
+    }
+    this.key = key || 'aaaaaaaaaa';
   }
 
   encode(value) {

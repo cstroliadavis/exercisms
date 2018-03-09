5922d7ba2393ad1a859e9152793b5abea9ccc146
Chris <Chris@Strolia-Davis.us>
2018-03-09 09:43:09 -0500

    Refactor working code for 'year divisible by 100, not divisible by 400: common year' test in leap, ecmascript task for easier maintenance.

--- ecmascript/leap/leap.js
+++ ecmascript/leap/leap.js
@@ -4,8 +4,9 @@ export default class Year {
     }
 
     isLeap() {
-        const isDivisBy4 = this.year % 4 === 0;
-        const isNotDivisBy100 = this.year % 100 !== 0;
+        const [isDivisBy4, isDivisBy100] = [4, 100]
+            .map(n => this.year % n === 0);
+        const isNotDivisBy100 = !isDivisBy100;
 
         return isNotDivisBy100 && isDivisBy4;
     }

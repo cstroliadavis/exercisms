fddf74780f23b086f44323050e97b51df8a9cf14
Chris <Chris@Strolia-Davis.us>
2018-03-11 11:06:53 -0400

    Make 'missing letters replaced by numbers' test for pangram, ecmascript exercise work

--- ecmascript/pangram/pangram.js
+++ ecmascript/pangram/pangram.js
@@ -1,9 +1,13 @@
+const HAS_LETTERS = [...'xet'];
+
 export default class Pangram {
   constructor(text){
     this.text = text;
   }
 
   isPangram(){
-    return !!this.text && this.text.includes('x');
+    return !!this.text &&
+      HAS_LETTERS
+        .every(l => this.text.includes(l));
   }
 }
